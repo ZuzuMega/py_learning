47edc805360993942019d6f92a8ee3b804a517d6
ZuzuMega <guk.5ergey@yandex.ru>
2022-04-30 00:03:43 +0300

    dz_3 was finished

--- les_2/dz_3.py
+++ les_2/dz_3.py
@@ -1,27 +1,47 @@
 
-
 from asyncio.sslproto import _DO_HANDSHAKE
+from posixpath import split
+from tkinter import Y
 
 
 comand = input("Для запуска программы напиши Y: ")
 
-textSource ="qweqwy"
+textSource = ""
+emailArray = []
+domainsArray = []
 
 if comand.lower() == 'y':
     with open('les_2\dz_3_files\source_data.txt', 'r', encoding='UTF-8') as fileSource:
         textSource = fileSource.read()
     textArray = textSource.split("\n")
+    # Получаем email-ы и домены
     for str in textArray:
-        ### ...
+        str = str.split("|")
+        # Email-ы
+        emailArray.append(str[1])
+        # Домены
+        email = str[1]
+        domen = email.split("@")
+        domainsArray.append(domen[1])
+        
+    # domensArray - теперь множество. Повторяющиеся элементы удалены
+    domainsArray = set(domainsArray)
+    for domain in domainsArray:
+        # Создаем файл с названием домена
+        file = open(f'les_2\dz_3_files\{domain}.txt', 'w', encoding='UTF-8')
+        print("\n" + f"Обрабатываем домен {domain}")
+        # Перебираем emails
+        for email in emailArray:
+            domainEmail = email.split("@")
+            # Если домен email совпадает с текущим доменом, то email записывается в файл
+            if domainEmail[1] == domain:
+                file.write(email+"\n")
+                print(f"{email} записан в файл домена {domain}")
+        file.close()
+    print("\n" + "Исходный файл обработан. Email-ы размещены по папкам")  
 else:
     print("Программа завершила работу")
 
-
-
-
-
-
-    # Zapis - w
 
 
 """
