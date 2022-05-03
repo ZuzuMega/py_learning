7520d4887aee662d11200f13c313ee5589a3661c
ZuzuMega <guk.5ergey@yandex.ru>
2022-05-03 22:14:09 +0300

    dz_4 was done

--- les_2/dz_4.py
+++ les_2/dz_4.py
@@ -1,23 +1,40 @@
 from cgitb import text
+from fileinput import filename
+from msvcrt import putch
+from os import stat
+import os
+import shutil
 from requests import request
 
 import requests
 
 links = []
+filesLinks = ["robots.txt", "sitemap.xml"]
 
 with open("les_2\dz_4_files\links.txt", "r", encoding="UTF-8") as sourceFile:
     links = sourceFile.read().split("\n")
 
 for link in links:
-    status = requests.get(str(link))
+    print(f"Сайт: {link}")
 
+    for fileName in filesLinks:
+        print(f"Файл: {fileName}")
 
+        # Запрос к файлу сайта в потоке
+        status = requests.get((str(link)+str(fileName)), stream=True)
 
-
-
-
-
-
+        # Если статус 200, то все норм
+        if  status.status_code == 200:
+           puthFile = (link+fileName).split("/")
+           puthFile = puthFile[2]+"-"+puthFile[3]
+           with open(f"les_2\dz_4_files\{puthFile}", 'wb') as fileResult:
+               status.raw.decode_content = True
+               shutil.copyfileobj(status.raw, fileResult)
+           print("OK")
+        else:
+          print("Ошибка: " + str(status.status_code))
+    print("\n")    
+    
 
 
 
@@ -26,3 +43,5 @@ for link in links:
 скачивает файлы robots.txt и sitemap.xml и сохраняет на диск.
  В случае если файл не найден, выводится сообщение об этом.
 """
+
+         
